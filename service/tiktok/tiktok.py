ac99ca45c692fc86a2c98bca608af73bea0a28cf
Аслан Мамедрзаев <aslan.mamedrzaev@gmail.com>
2026-01-27 12:16:30 +0400

    fix 3

--- service/tiktok/tiktok.py
+++ service/tiktok/tiktok.py
@@ -82,15 +82,6 @@ class TikTokAPI:
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
                 'Accept-Language': 'en-US,en;q=0.9',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-fetch-dest': 'document',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-site': 'none',
-                'sec-fetch-user': '?1',
-                'upgrade-insecure-requests': '1',
             }
             async with httpx.AsyncClient() as client:
                 response = await client.get(self.link, follow_redirects=True, headers=headers)
